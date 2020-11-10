823443c71b221c5ba53165068fbf7a9342554cfd
Jorn Letnes <JLetnes@slb.com>
2020-11-10 08:59:48 +0100

    simple console app that runs a heartbeat in another thread

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello main thread!");
+
+            ThreadStart childref = CallToChildThread;
+            Thread childThread = new Thread(childref);
+            childThread.Start();
+        }
+
+        public static void CallToChildThread()
+        {
+            Console.WriteLine("My heart beat starts");
+
+            int pulse = 500;
+            int numHeartbeats = 0;
+            while (true)
+            {
+                Console.WriteLine("Heartbeat "+ numHeartbeats);
+                numHeartbeats++;
+                Thread.Sleep(pulse);
+            }
+        }
+    }
+}
