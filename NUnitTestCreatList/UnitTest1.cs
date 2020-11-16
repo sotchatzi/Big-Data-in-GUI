d3f20f34871e4c8554353558ea9b6b1077420bbd
ssalniyat <shuyiching@yahoo.com>
2020-11-16 16:33:19 +0800

    NUNIT test

---
+++ NUnitTestCreatList/UnitTest1.cs
@@ -0,0 +1,33 @@
+using Create_List_WPF;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NUnitTestCreatList
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestForGenerator()
+        {
+            IGenerator generator = new ForLoopGenerator();
+            IEnumerable<ItemList> result = generator.Generate(10000);
+            ICollection collection = result as ICollection;
+            Assert.AreEqual(10000, collection.Count);
+        }
+
+        //[Test]
+        public void TestYieldGenerator()
+        {
+            var generator = new YieldGenerator();
+            var result = generator.Generate(10000);
+            ICollection collection = result as ICollection;
+            Assert.AreEqual(10000, collection.Count);
+        }
+    }
+}
