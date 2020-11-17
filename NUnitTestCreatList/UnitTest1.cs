7788bd205cbc93002f9c4be42646b77366a918fb
sotchatzi <sotchatzi@github.com>
2020-11-17 17:51:06 +0100

    Introduce fail test to check nunit in the pipeline

--- NUnitTestCreatList/UnitTest1.cs
+++ NUnitTestCreatList/UnitTest1.cs
@@ -33,5 +33,18 @@ namespace NUnitTestCreatList
             }
             Assert.AreEqual(10000, total);
         }
+
+        [Test]
+        public void TestFail()
+        {
+            var generator = new YieldGenerator();
+            var result = generator.Generate(10000);
+            int total = 0;
+            foreach(ItemList item in result)
+            {
+                total += 1;
+            }
+            Assert.AreEqual(10010, total);
+        }
     }
 }
