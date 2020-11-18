644cebc8c4c94a8728771ea97bd9f69e97016f8e
Jorn Letnes <JLetnes@slb.com>
2020-11-18 09:19:52 +0100

    resolved some build errors

--- WpfApp/PopulateGUIOneByOne.cs
+++ WpfApp/PopulateGUIOneByOne.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows;
-using System.Diagnostics;
+using Generators;
 
 namespace Create_List_WPF
 {
