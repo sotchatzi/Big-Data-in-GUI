87d8c3373889d7e3bf2ddffa1c024a3e23b2a7e8
sotchatzi <sotchatzi@github.com>
2020-11-21 19:58:47 +0100

    Tests fixed and performance test project added

--- DotNetCoreTests/ItemListTest.cs
+++ DotNetCoreTests/ItemListTest.cs
@@ -7,23 +7,37 @@ namespace DotNetCoreTests
 {
     public class ItemListTest
     {
-        [Test, Description("Tests that the get() works")]
-        public void ItemListGet([Random(1, 1000, 1)] int index)
+        [Test, Description("Tests that the get() for AnIndex field works")]
+        public void ItemListGetAnIndex([Random(1, 1000, 1)] int index)
         {
             ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString() };
             Assert.That(anItemList.AnIndex, Is.Not.Null);
+        }
+
+        [Test, Description("Tests that the get() for AString field works")]
+        public void ItemListGetAString([Random(1, 1000, 1)] int index)
+        {
+            ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString() };
             Assert.That(anItemList.AString, Is.Not.Null);
         }
 
-        [Test, Description("Tests that the set() works")]
-        public void ItemListSet([Random(1, 1000, 1)] int index)
+        [Test, Description("Tests that the set() for AnIndex field works")]
+        public void ItemListSetAnIndex([Random(1, 1000, 1)] int index)
         {
             ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString() };
 
             anItemList.AnIndex = 2;
-            anItemList.AString = "updated value";
 
             Assert.AreEqual(anItemList.AnIndex, 2);
+        }
+
+        [Test, Description("Tests that the set() for AStringworks")]
+        public void ItemListSetAString([Random(1, 1000, 1)] int index)
+        {
+            ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString() };
+
+            anItemList.AString = "updated value";
+
             Assert.AreEqual(anItemList.AString, "updated value");
         }
 
