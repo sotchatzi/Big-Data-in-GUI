6540fd88b917932fe8c00357d329feaf25b0f422
ssalniyat <shuyiching@yahoo.com>
2020-11-24 15:57:19 +0800

    Add time out generator and test all the generators

--- WpfApp/PopulateGUIOneByOne.cs
+++ WpfApp/PopulateGUIOneByOne.cs
@@ -6,19 +6,30 @@ using System.Windows.Threading;
 using System.Windows.Controls;
 using System.Windows;
 using Generators;
+using System.Diagnostics;
 
 namespace Create_List_WPF
 {
     class PopulateGUIOneByOne
     {
-        public void PopulateGUI(TextBox NumberOfList, ListBox listBox, ProgressBar progressBar)
+        public void PopulateGUI(TextBox NumberOfList, ListBox listBox, ProgressBar progressBar, TextBlock elapsedTime)
         {
             int size = Convert.ToInt32(NumberOfList.Text);
-
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
             var mySource = Enumerable.Range(1, size).ToList();
             Task.Factory.StartNew(() => { 
                 Generate(mySource, size, listBox, progressBar); 
             });
+            //while (progressBar.Value < 99)
+            //{
+            //    continue;
+            //};
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+
+            double minutesToSeconds = ts.Minutes * 60;
+            elapsedTime.DataContext = new TextboxText() { seconds = ts.Seconds + minutesToSeconds, milliseconds = ts.Milliseconds };
         }
 
         private void Generate(List<int> values, int size, ListBox listBox, ProgressBar progressBar)
