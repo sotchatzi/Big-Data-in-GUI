bf1f25f580f5f999a1d17db53829404e2ae9da8a
ssalniyat <shuyiching@yahoo.com>
2020-11-30 15:56:10 +0800

    Add matrix to the GUI

--- DotNetCoreTests/ItemListTest.cs
+++ DotNetCoreTests/ItemListTest.cs
@@ -10,21 +10,28 @@ namespace DotNetCoreTests
         [Test, Description("Tests that the get() for AnIndex field works")]
         public void ItemListGetAnIndex([Random(1, 1000, 1)] int index)
         {
-            ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString() };
+            ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString(), AFlag = 0};
             Assert.That(anItemList.AnIndex, Is.Not.Null);
         }
 
         [Test, Description("Tests that the get() for AString field works")]
         public void ItemListGetAString([Random(1, 1000, 1)] int index)
         {
-            ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString() };
+            ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString(), AFlag = 0 };
             Assert.That(anItemList.AString, Is.Not.Null);
+        }
+
+        [Test, Description("Tests that the get() for AFlag field works")]
+        public void ItemListGetAFlag([Random(1, 1000, 1)] int index)
+        {
+            ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString(), AFlag = 0 };
+            Assert.That(anItemList.AFlag, Is.Not.Null);
         }
 
         [Test, Description("Tests that the set() for AnIndex field works")]
         public void ItemListSetAnIndex([Random(1, 1000, 1)] int index)
         {
-            ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString() };
+            ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString(), AFlag = 0 };
 
             anItemList.AnIndex = 2;
 
@@ -34,11 +41,21 @@ namespace DotNetCoreTests
         [Test, Description("Tests that the set() for AStringworks")]
         public void ItemListSetAString([Random(1, 1000, 1)] int index)
         {
-            ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString() };
+            ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString(), AFlag = 0 };
 
             anItemList.AString = "updated value";
 
             Assert.AreEqual(anItemList.AString, "updated value");
+        }
+
+        [Test, Description("Tests that the set() for AFlagworks")]
+        public void ItemListSetAFlag([Random(1, 1000, 1)] int index)
+        {
+            ItemList anItemList = new ItemList() { AnIndex = index, AString = RandomUtil.GetRandomString(), AFlag = 0 };
+
+            anItemList.AFlag = 0;
+
+            Assert.AreEqual(anItemList.AFlag, 0);
         }
 
     }
