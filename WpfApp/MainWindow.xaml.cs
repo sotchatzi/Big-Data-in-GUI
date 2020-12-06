0be86973dc296bc5cd9609a5bb1691eb9c8d76d5
ssalniyat <shuyiching@yahoo.com>
2020-12-06 14:44:39 +0800

    fix typo

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -160,11 +160,11 @@ namespace Create_List_WPF
             string arr = strArray[0];
             string DirReport = arr + @"PerformanceTests\bin\Debug\netcoreapp3.1\reportTime.txt";
             //Console.WriteLine(DirReport);
-            string textFile = @"C:\Users\DELL\source\repos\sotchatzi\Big-Data-in-GUI\PerformanceTests\bin\Debug\netcoreapp3.1\reportTime.txt";
+            //string textFile = @"C:\Users\DELL\source\repos\sotchatzi\Big-Data-in-GUI\PerformanceTests\bin\Debug\netcoreapp3.1\reportTime.txt";
             FileStream fs;
             if (File.Exists(DirReport))
             {
-                fs = new FileStream(textFile, FileMode.Open, FileAccess.Read);
+                fs = new FileStream(DirReport, FileMode.Open, FileAccess.Read);
                 using (fs)
                 {
                     TextRange text = new TextRange(richTextBox1.Document.ContentStart, richTextBox1.Document.ContentEnd);
